8c56494a09d362ab5bf08fe49b12a515413306de
Nasser <nmaif7788@hotmail.com>
2023-03-15 00:33:56 +0300

    second commit

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Button,
   FlatList,
@@ -9,14 +10,44 @@ import {
 } from "react-native";
 
 export default function App() {
+  const [newTask, setNewTask] = useState("");
+  const [allTasks, setAllTasks] = useState([]);
+
+  function textChange(text) {
+    setNewTask(text);
+  }
+
+  function addingNewTask() {
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      task: newTask,
+    };
+
+    setAllTasks((oldItem) => [...oldItem, item]);
+    setNewTask("");
+  }
+
+  function deleteFunc(id) {
+    const newArr = allTasks.filter((elmID) => {
+      if (elmID.id !== id) return elmID;
+    });
+    setAllTasks(newArr);
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.v1}>
-        <TextInput style={styles.inputSt} placeholder="Enter your name" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputSt}
+          placeholder="Enter your name"
+          value={newTask}
+          onChangeText={textChange}
+        />
         <Pressable
+          onPress={addingNewTask}
           style={{
             padding: 10,
-            backgroundColor: "red",
+            backgroundColor: "#00a6fb",
             alignItems: "center",
             justifyContent: "center",
             borderRadius: 10,
@@ -32,6 +63,28 @@ export default function App() {
           </Text>
         </Pressable>
       </View>
+
+      <View style={styles.taskContainer}>
+        {allTasks.map((elm) => (
+          <View
+            key={elm.id}
+            style={{
+              flex: 1,
+              justifyContent: "space-evenly",
+              flexDirection: "row",
+              alignItems: "center",
+              // height: 20,
+              maxHeight: 70,
+              borderWidth: 1,
+              marginTop: 10,
+              paddingHorizontal: 10,
+            }}
+          >
+            <Text>{elm.task}</Text>
+            <Button title="Delete" onPress={() => deleteFunc(elm.id)} />
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
@@ -39,24 +92,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 60,
-    padding: 10,
-    height: 300,
-    backgroundColor: "lightgray",
+    paddingTop: 55,
+    paddingHorizontal: 13,
   },
-  v1: {
+  inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-around",
-    height: 50,
+    alignItems: "center",
+    height: 70,
     padding: 5,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
 
   inputSt: {
-    backgroundColor: "white",
     color: "black",
-    width: "40%",
-    height: "100%",
+    width: "60%",
+    borderWidth: 1,
+    borderColor: "#cccccc",
     padding: 5,
     borderRadius: 10,
+  },
+
+  taskContainer: {
+    flex: 7,
+    padding: 10,
   },
 });
